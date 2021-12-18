7ac8c5ecd7d5e6e24ed1427923c194386ba29d3f
artleoniss <90518983+artleoniss@users.noreply.github.com>
2021-12-19 01:47:49 +0700

    Начало работы

---
+++ js/library.js
@@ -0,0 +1,457 @@
+let library = {
+      Музыка: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Ультра: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Разное: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Организация: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Кино: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Люди: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+      Числа: {
+            100: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            200: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            300: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            400: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            500: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            600: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+            700: {
+                  question: "",
+                  variants: [
+                        "",
+                        "",
+                        "",
+                        "",
+                  ]
+            },
+      },
+}
